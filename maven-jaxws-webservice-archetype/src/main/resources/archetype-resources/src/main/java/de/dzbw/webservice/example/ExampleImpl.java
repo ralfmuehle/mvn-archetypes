28a1d99869333ac2df45e03a29b108cb59f7bcc7
Ralf Muehle <muehle.ralf@gmail.com>
2014-08-16 12:45:42 +0200

    added archetypes

---
+++ maven-jaxws-webservice-archetype/src/main/resources/archetype-resources/src/main/java/de/dzbw/webservice/example/ExampleImpl.java
@@ -0,0 +1,13 @@
+package de.dzbw.webservice.example;
+
+import de.dzbw.webservice.example.Example;
+
+public class ExampleImpl implements Example {
+
+	@Override
+	public String newOperation(final String in) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
